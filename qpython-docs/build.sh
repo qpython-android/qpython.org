08c99cbb8288c9580db9df634ad9cea04d0a2888
River <riverfor@gmail.com>
2020-01-25 22:23:20 +0800

    Fix broken links

--- qpython-docs/build.sh
+++ qpython-docs/build.sh
@@ -7,4 +7,4 @@ make html
 
 cd build/html && find . -name "*.html" -exec python2 ../../add-analytics.py {} \; && find . -name "*.html" -exec sed -i -e 's/_static/static/g;s/_images/images/g' {} \; && find . -name "*-e" -exec rm {} \; && mv _static static && mv _images  images
 
-cd ../.. && mv build/html  ../docs && cp CNAME ../docs  && cp -r quick-start ../docs && cp favicon.ico  ../docs && cp index.html ../docs && cp community.html ../docs && cp -r market ../docs
+cd ../.. && mv build/html  ../docs && cp CNAME ../docs  && cp -r quick-start ../docs && cp favicon.ico  ../docs && cp index.html ../docs && cp community.html discord.html privacy.html qlua-privacy.html qlua-rate.html ../docs && cp -r market ../docs
