b83fab372e4d6ed82ea2b5e218a42438380556d0
黄佰荣 <huangbairong@quseit.com>
2025-02-25 17:07:55 +0800

    代码还原

--- qpython-docs/add-analytics.py
+++ qpython-docs/add-analytics.py
@@ -1,13 +1,10 @@
-import sys, os
-
-with open(sys.argv[1], 'r', encoding='gbk') as f, \
-     open('./qpydoc.tmp', 'w', encoding='gbk') as g, \
-     open(os.path.dirname(os.path.abspath(__file__)) + '/extra.txt', 'r', encoding='gbk') as e:
+import sys,os
+with open(sys.argv[1], 'r') as f, open('./qpydoc.tmp', 'wb') as g, open(os.path.dirname(os.path.abspath(__file__))+'/extra.txt','r') as e:
     pth = sys.argv[1][1:]
-    extra = "".join(e.readlines()).replace("{{PTH}}", pth)
+    extra = "".join(e.readlines()).replace("{{PTH}}",pth)
     g.write('\n'.join(
         filter(lambda s: len(s),
                map(lambda s:
-                   ('', extra + "<hr/>")[s == '<div role="contentinfo">'] + s,
+                       ('',extra+"<hr/>")[s=='<div role="contentinfo">']+s,
                    map(str.strip, f.readlines())))))
 os.rename('./qpydoc.tmp', sys.argv[1])
