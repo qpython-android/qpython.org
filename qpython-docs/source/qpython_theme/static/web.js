1cf4b335b978f13875ac01251d5d33497a3f7f44
黄佰荣 <huangbairong@quseit.com>
2025-02-28 14:32:58 +0800

    添加导航

--- qpython-docs/source/qpython_theme/static/web.js
+++ qpython-docs/source/qpython_theme/static/web.js
@@ -2,12 +2,21 @@ jQuery(function($) {
     submenu_init();
 });
 
-// mobile 二级目录的点击功能
+// 二级目录的点击功能
 function submenu_init() {
     // 判断当前屏幕
     jQuery('#example-navbar-collapse li.li-sub').click(function (event) {
         event.stopPropagation(); // 阻止事件冒泡
         let ele = jQuery(this).find('.submenu');
+
+        // 第一次点击需要显示(因为存在兼容性BUG)
+        if(!ele.data('click_first')){
+            ele.show();
+            ele.data('click_first',true)
+            return true;
+        }
+
+        console.log(ele.data('click_first'));
         // 判断显示隐藏
         if (ele.is(':visible')) {
             ele.hide();
